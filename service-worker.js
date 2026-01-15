8d27eb115b4a1fefe9ad39d68418b47b2cf0d275
aslessh98 <dailydiaries698@gmail.com>
2026-01-15 12:57:39 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,14 @@
-self.addEventListener('push', e => {
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/') // Opens your app when they click the notification
+    );
+});
+
+/*self.addEventListener('push', e => {
   const data = e.data.json();
   self.registration.showNotification(data.title, {
     body: data.body,
     icon: 'icon-192.png'
   });
-});
+});*/
